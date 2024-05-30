4377f160b851643db66f755cb2858a40f9255170
YesYesTheDev <118150960+YesYesTheDev@users.noreply.github.com>
2024-05-30 13:21:13 -0500

    Updated Welcome message. Added Leave Message

--- cogs/welcomeCog.py
+++ cogs/welcomeCog.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+WELCOME_CHANNEL = os.getenv('WELCOME_CHANNEL')
+LEAVE_CHANNEL = os.getenv('LEAVE_CHANNEL')
 
 class WelcomeCog(commands.Cog):
     def __init__(self, bot):
@@ -11,9 +18,17 @@ class WelcomeCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = discord.utils.get(member.guild.text_channels, name='general')
+        channel_id = WELCOME_CHANNEL
+        channel = self.bot.get_channel(channel_id)
         if channel:
             await channel.send(f'Welcome to the server, {member.mention}! We are glad to have you here.')
+    
+    @commands.Cog.listener()
+    async def on_member_leave(self, member):
+        channel_id = LEAVE_CHANNEL
+        channel = self.bot.get_channel(channel_id)
+        if channel:
+            await channel.send(f'We are sad to see you leave the server, {member.mention}! Please come back to us.')
 
 def setup(bot):
     bot.add_cog(WelcomeCog(bot))
